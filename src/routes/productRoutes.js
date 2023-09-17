6a26eb1fc4dafcbc31f366cbcc0105eb0427d21b
alejoc0314 <alejoc0314@gmail.com>
2023-09-17 01:07:18 -0500

    Fix products routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const ProductController = require('../controllers/ProductController');
 
 // Ruta para obtener todos los roles
-router.get('/', ProductController.getAllProducts);
+router.get('/products', ProductController.getAllProducts);
 
 // Ruta para obtener un rol por ID
-router.get('/:id', ProductController.getProductById);
+router.get('/products/:id', ProductController.getProductById);
 
 // Ruta para crear un nuevo rol
-router.post('/', ProductController.createProduct);
+router.post('/products', ProductController.createProduct);
 
 // Ruta para actualizar un rol por ID
-router.put('/:id', ProductController.updateProduct);
+router.put('/products/:id', ProductController.updateProduct);
 
 // Ruta para eliminar un rol por ID
-router.delete('/:id', ProductController.deleteProduct);
+router.delete('/products/:id', ProductController.deleteProduct);
 
 module.exports = router;
